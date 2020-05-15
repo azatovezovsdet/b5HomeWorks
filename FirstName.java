7437696b61939e281c02fe1b8ecaeb0c3982f2dc
azatovezovsdet <65377934+azatovezovsdet@users.noreply.github.com>
2020-05-14 20:49:04 -0500

    Add files via upload

---
+++ FirstName.java
@@ -0,0 +1,14 @@
+public class FirstName {
+	
+	public static void main(String[] args){
+
+		String name = "John";
+		System.out.println(name);
+
+		name = "David";
+		System.out.println(name);
+
+	}
+
+
+}
